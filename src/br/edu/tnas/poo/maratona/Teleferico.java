c7dbacc990d62efb5d708cd143361254aebe65e5
Thiago Nascimento <nascimenthiago@gmail.com>
2019-04-10 14:30:06 -0300

    Maratona 2017 - Teleferico

---
+++ src/br/edu/tnas/poo/maratona/Teleferico.java
@@ -0,0 +1,38 @@
+package br.edu.tnas.poo.maratona;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Maratona de Programação da SBC - 2017
+ * Problema C - Teleferico
+ * @author Thiago Nascimento <nascimenthiago@gmail.com>
+ */
+public class Teleferico {
+    
+    public int calcularViagens(File input) {
+        
+        try {
+            Scanner scan = new Scanner(input);
+            int capacidade = scan.nextInt();
+            int alunos = scan.nextInt(); 
+            scan.close();
+            
+            --capacidade; // lugar do monitor
+            
+            int telefericoCheio = alunos / capacidade;
+            int excedente = alunos % capacidade;
+            
+            if (excedente == 0)
+                return telefericoCheio;
+            else 
+                return telefericoCheio + 1;
+            
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+            
+        return -1;
+    }
+}
--- test/br/edu/tnas/poo/maratona/TelefericoTeste.java
+++ test/br/edu/tnas/poo/maratona/TelefericoTeste.java
@@ -1,5 +1,7 @@
 package br.edu.tnas.poo.maratona;
 
+import static br.edu.tnas.poo.maratona.JogoEstrategiaTeste.INTPUT_DIR;
+import static br.edu.tnas.poo.maratona.JogoEstrategiaTeste.OUTPUT_DIR;
 import br.edu.tnas.poo.util.FileHelper;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -9,29 +11,29 @@ import java.util.Scanner;
  *
  * @author Thiago Nascimento <nascimenthiago@gmail.com>
  */
-public class JogoEstrategiaTeste {
+public class TelefericoTeste {
     
-    public static final String INTPUT_DIR = "./maratona-input/JogoEstrategia/";
-    public static final String OUTPUT_DIR = "./maratona-output/JogoEstrategia/";
-
+    public static final String INTPUT_DIR = "./maratona-input/Teleferico/";
+    public static final String OUTPUT_DIR = "./maratona-output/Teleferico/";
+    
     public static void main(String[] args) {
         
-        JogoEstrategia jogo = new JogoEstrategia();
+        Teleferico teleferico = new Teleferico();
         FileHelper inputFileHelper = new FileHelper(INTPUT_DIR);
         
         inputFileHelper.getFiles().forEach(f -> {
             File inputFile = new File(f.getAbsolutePath());
-            int vencedor = jogo.rodar(inputFile);
+            int numViagens = teleferico.calcularViagens(inputFile);
             File outputFile = new File(OUTPUT_DIR.concat(inputFile.getName()));
             try {
                 try (Scanner scan = new Scanner(outputFile)) {
                     int gabarito = scan.nextInt();
-                    if (vencedor == gabarito) {
+                    if (numViagens == gabarito) {
                         System.out.println(inputFile.getName() + " --- OK");
                     }
                     else {
                         System.out.println(inputFile.getName() + " --- Falha");
-                        System.out.printf("Esperado: %d, Encontrado: %d\n", gabarito, vencedor);
+                        System.out.printf("Esperado: %d, Encontrado: %d\n", gabarito, numViagens);
                     }
                     scan.close();
                 }
